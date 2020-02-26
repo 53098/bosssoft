24af348e7819580a48c32718a6e2b0d3eddc152d
wenya0110 <1759768433@qq.com>
2020-02-26 14:15:18 +0800

    merge develop to master

--- hello.c
+++ hello.c
@@ -2,5 +2,7 @@
 #include <stdio.h>
 int main(int arg,char* argv[]){
 	printf("inited by SCM.\n");
-	 printf("hhhhhhhhhhhhhhhhh.\n");
+	printf("function02\n");
+        printf("function01\n");
+	printf("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.\n");
 }
